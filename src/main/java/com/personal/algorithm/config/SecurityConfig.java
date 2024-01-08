2870b05ce747f38ad42deba36a6c96d83fd631fa
이종민 <john0513@naver.com>
2024-01-08 18:12:18 +0900

    feat: secret key make and security version update

--- src/main/java/com/personal/algorithm/config/SecurityConfig.java
+++ src/main/java/com/personal/algorithm/config/SecurityConfig.java
@@ -5,14 +5,18 @@ import com.personal.algorithm.util.jwt.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -38,8 +42,19 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests((authz) -> authz.antMatchers("/admin/register", "/admin/login").permitAll().antMatchers("/admin/**").hasRole("ADMIN").anyRequest().authenticated()).addFilterBefore(new JwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-
+        http
+                .csrf(AbstractHttpConfigurer::disable) // CSRF 비활성화
+                .formLogin(Customizer.withDefaults()) // FormLogin 비활성화
+                .authorizeHttpRequests(
+                        authorizeRequest ->
+                                authorizeRequest
+                                .requestMatchers(AntPathRequestMatcher.antMatcher("/admin/**"))
+                                .authenticated() // /admin/** 요청 권한필요
+                                .requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**"))
+                                .permitAll()) // h2-console 의 경로에 대한 권한설정
+                .headers(
+                        headersConfigurer ->
+                                headersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)); // h2 접근을 위한 SameOrigin (프레임 허용) & 보안 강화 설정
         return http.build();
     }
 
